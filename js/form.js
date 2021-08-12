201515cd99a9462087b761618e420a16a59858b6
Abhinandan singh dahwal <70463036+AbhinandanSinghDadhwal@users.noreply.github.com>
2021-08-12 15:08:39 +0530

    upload all files

--- js/form.js
+++ js/form.js
@@ -1,9 +1,31 @@
 class Screen{
     constructor(){
         this.start = createImg("images/maxresdefault.png")
-        //this.how  = createImg()
+
+        this.bank = createImg("images/vault.jpg")
+        this.thief = createImg("images/giphy.gif")
+        this.police = createImg("images/police.webp")
+
+        this.storyText1=createElement("h4") 
+        this.storyText2=createElement("h4") 
+
+        this.play=createImg("images/play.png")
+        this.how=createImg("images/instructions.png")
+        this.howText = createImg("images/how.png")
+        this.rule1 = createElement("h4")
+        this.rule2 = createElement("h4")
+        this.rule3 = createElement("h4")
+        this.rule4 = createElement("h4")
+        this.rule5 = createElement("h4")
+        this.rule6 = createElement("h4")
     }
     startScreen(){
+        this.how.hide();
+        this.howText.hide();
+        this.thief.hide();
+        this.police.hide();
+        this.bank.hide();
+        this.play.hide();
         this.start.position(250,380);
         this.start.size(650,300)
         this.start.mouseClicked(()=>{
@@ -11,8 +33,92 @@ class Screen{
         })
     }
     storyScreen(){
-        this.start.hide()
+        this.howText .hide()
+        this.start.hide();
+        this.how.show();
+        this.bank.show();
+        this.police.show();
+        this.play.hide();
+
+        this.thief.show();
+        this.thief.position(450,150);
+
+    
+        this.police.position(80,180);
+        this.police.size(250,250)
+
+        this.bank.position(0,0)
+        this.bank.size(800,600)
+
+        this.storyText1.html("This is corrupt bank full of the black money and gold")
+        this.storyText1.position(10,10)
+        this.storyText1.style("color","navy")
+        this.storyText1.style("font-size","24px")
+
+        this.storyText2.html("Robin Hood is a saviour. Help Robin Hood to escape from the police")
+        this.storyText2.position(10,70)
+        this.storyText2.style("color","navy")
+        this.storyText2.style("font-size","24px")
+               
+        this.how.position(100,420);
+        this.how.size(200,200)
+        this.how.mouseClicked(()=>{
+            gameState="rules";
+        })
 
 
+    }
+
+    ruleScreen(){  
+        this.howText.show();  
+        this.bank.hide();
+        this.thief.hide();
+        this.police.hide();
+        this.storyText1.hide();
+        this.storyText2.hide();
+        this.how.hide();
+        this.play.show();
+       
+        this.play.position(500,350)
+        this.play.size(250,250)
+
+        this.howText.position(50,20);
+        this.howText.size(250,250);
+
+        this.rule1.html("1. Press Space to jump")
+        this.rule1.position(400, 40)
+        this.rule1.style("color","yellow")
+        this.rule1.style("font-size","24px")
+
+        this.rule2.html("2. Avoid Obstacles")
+        this.rule2.position(400, 80)
+        this.rule2.style("color","yellow")
+        this.rule2.style("font-size","24px")
+
+        this.rule3.html("3. Collect coins")
+        this.rule3.position(400, 120)
+        this.rule3.style("color","yellow")
+        this.rule3.style("font-size","24px")
+
+        this.rule4.html("4. Will get 3 chances")
+        this.rule4.position(400, 160)
+        this.rule4.style("color","yellow")
+        this.rule4.style("font-size","24px")
+
+        this.rule5.html("5. Collect cups on scoring 100, 200, 300 points")
+        this.rule5.position(300, 200)
+        this.rule5.style("color","yellow")
+        this.rule5.style("font-size","24px")
+
+
+        this.rule6.html("6. Police will catch you on losing all chances")
+        this.rule6.position(300, 240)
+        this.rule6.style("color","yellow")
+        this.rule6.style("font-size","24px")
+
+        this.play.mouseClicked(()=>{
+            gameState="game";
+        })
+        
     }
 }
--- sketch.js
+++ sketch.js
@@ -3,20 +3,68 @@ var gameState = "start";
 var bgimg;
 var screen;
 
+var city_bg,city_night
+
+var invisibleground
+
 function preload(){
 
   
   //trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   bgimg = loadImage("images/bgimage.png");
+
+  how = loadImage("images/BG.webp")
   
+  city_bg = loadImage("images/bg3.jpg");
+  city_night = loadImage("images/bg3.jpg");
+
+  thief_img = loadImage("images/thief.gif")
+
+  // thief_img = loadAnimation("images/thief1.png", "images/thief2.png")
+
+  police_img = loadImage("images/police-car-71.gif")
+  
+
+  //gameOverImg = loadImage("images/gameOver.png");
+  //restartImg = loadImage("images/restart.png");
+  jumpSound = loadSound("images/jump.mp3");
+
+  coin_img = loadImage("images/coin.png")
+  diamond_img = loadImage("images/diamons.png")
+  obstacle1 = loadImage("images/car.png")
+  obstacle2 = loadImage("images/bus.png")
  
 }
 
+
+
 function setup() {
+
   createCanvas(800, 600);
-  screen= new Screen()
- 
+  screen= new Screen();
+  
+  city = createSprite(400, 300,800,600)
+  city.addImage(city_bg)
+  city.height = 600
+  
+  invisibleground = createSprite(400, 600, 800, 5)
+  invisibleground.visible = false
+
+  thief = createSprite(300,530,20,50)
+  thief.addImage(thief_img)
+  thief.scale = 0.7;
+
+  police = createSprite(100,570,20,50)
+  police.addImage(police_img)
+  police.scale = 0.2
+  
+  console.log(thief.y)
+
+   coinGroup= new Group()
+  obstaclesGroup = new Group()
+
 }
+
 
 function draw() {
   
@@ -26,30 +74,69 @@ function draw() {
       
   }
   if (gameState === "story"){
-    background(0)
+    background(02);
     screen.storyScreen()
   }
- 
+
+  if(gameState==="rules"){
+    background(how);
+    screen.ruleScreen()
+  }
+
+  if(gameState==="game"){
+    background(0);
+    screen.play.hide()
+    screen.howText.hide()
+    screen.rule1.hide()
+    screen.rule2.hide()
+    screen.rule3.hide()
+    screen.rule4.hide()
+    screen.rule5.hide()
+    screen.rule6.hide()
+    
+    game()
+
+    drawSprites()
+  }
+  
 }
 
-function spawnClouds() {
+function game(){
+
+  city.velocityX = -6
+
+  if(city.x < 0){
+      city.x = city.width/2
+  }
+
+  if(keyDown("space")&& thief.y>=400 ) {
+    thief.velocityY = -12;
+    jumpSound.play();
+  }
+
+
+  thief.velocityY = thief.velocityY + 0.8
+  thief.collide(invisibleground);
+  spawnCoins()
+  //spawnObstacles()
+
+}
+
+function spawnCoins() {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,120,40,10);
-    cloud.y = Math.round(random(80,120));
-    cloud.addImage(cloudImage);
-    cloud.scale = 0.5;
-    cloud.velocityX = -3;
+    var coin = createSprite(800,120,40,10);
+    coin.y = Math.round(random(200,500));
+    coin.addImage(coin_img);
+    coin.scale = 0.5;
+    coin.velocityX = -3;
     
      //assign lifetime to the variable
-    cloud.lifetime = 200;
+    coin.lifetime = 200;
     
-    //adjust the depth
-    cloud.depth = trex.depth;
-    trex.depth = trex.depth + 1;
-    
+      
     //add each cloud to the group
-    cloudsGroup.add(cloud);
+    coinGroup.add(coin);
   }
   
 }
@@ -57,7 +144,8 @@ function spawnClouds() {
 function spawnObstacles() {
   if(frameCount % 60 === 0) {
     var obstacle = createSprite(600,165,10,40);
-    obstacle.velocityX = -4;
+    //obstacle.debug = true;
+    obstacle.velocityX = -(6 + 3*score/100);
     
     //generate random obstacles
     var rand = Math.round(random(1,6));
@@ -84,3 +172,8 @@ function spawnObstacles() {
     obstaclesGroup.add(obstacle);
   }
 }
+
+
+
+
+
